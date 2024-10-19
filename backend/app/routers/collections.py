71f0afd227dfee8c6e5c57c3bbff22c7fd1006a9
Anton Tsoy <tcoy210@gmail.com>
2024-10-19 23:48:36 +0700

    Finished review corrections of code

--- backend/app/routers/collections.py
+++ backend/app/routers/collections.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Body, Depends, HTTPException
-from sqlalchemy import delete, insert
+from sqlalchemy import delete, insert, select, and_
 from sqlalchemy.orm import Session
 
 from app import crud, schemas, models
@@ -42,17 +42,31 @@ def create_collection(
     return crud.collection.create_collection(db=db, collection=collection, user_id=user_id)
 
 
+def check_connections(collection_id: int, cards: list[int], db: Session) -> bool:
+    for card_id in cards:
+        query = (
+            select(models.association_table).where(models.association_table.c.collection_id == collection_id).where(
+                models.association_table.c.card_id == card_id
+            )
+        )
+        res = db.execute(query)
+        if len(res.all()) > 0:
+            return False
+    return True
+
 @router.post("/{collection_id}/pair")
 def set_card_collection_connection(
         collection_id: int, cards: Annotated[list[int], Body], db: Session = Depends(get_db)
 ):
-    for card_id in cards:
-        db.execute(
-            insert(models.association_table).values(card_id=card_id, collection_id=collection_id)
-        )
-        db.flush()
-    db.commit()
-    return "Done"
+    if check_connections(collection_id, cards, db):
+        for card_id in cards:
+            db.execute(
+                insert(models.association_table).values(card_id=card_id, collection_id=collection_id)
+            )
+            db.flush()
+        db.commit()
+        return "Done"
+    raise HTTPException(status_code=400, detail="Rows of connections already exist")
 
 
 @router.delete("/{collection_id}/unpair")
@@ -61,9 +75,10 @@ def delete_card_collection_connection(
 ):
     for card_id in cards:
         db.execute(
-            delete(models.association_table).where(
-                models.association_table.c.card_id == card_id and models.association_table.c.collection_id == collection_id
-            )
+            delete(models.association_table).filter(and_(
+                models.association_table.c.collection_id == collection_id,
+                models.association_table.c.card_id == card_id
+            ))
         )
         db.flush()
     db.commit()
