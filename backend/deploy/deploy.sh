c7317a14b51760c81303fa05053d71bae5a2d3d0
kirill-serve <kozlov.kirill.v.work@gmail.com>
2024-11-03 19:29:59 +0000

    Fix composing

--- backend/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker compose up -d
-docker compose exec minio bash /setup.sh
--- backend/deploy/minio_config/setup.sh
+++ backend/deploy/minio_config/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-until curl -s $MINIO_SERVER_HOSTNAME; do
-  echo "Waiting for MinIO to start..."
-  sleep 5
-done
+#until curl -s $MINIO_SERVER_HOSTNAME; do
+#  echo "Waiting for MinIO to start..."
+#  sleep 5
+#done
 mc alias set local $MINIO_SERVER_HOSTNAME $MINIO_ADMIN_LOGIN $MINIO_ADMIN_PASSWORD
 mc admin user add local $MINIO_LOGIN $MINIO_PASSWORD
 mc admin policy attach local readwrite --user $MINIO_LOGIN
