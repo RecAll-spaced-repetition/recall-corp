0043291d910d319ef4853b190e7d4a1d0ad1347e
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-11-04 01:56:34 +0700

    Add composing for minio container

---
+++ backend/deploy/minio_config/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+until curl -s $MINIO_SERVER_HOSTNAME; do
+  echo "Waiting for MinIO to start..."
+  sleep 5
+done
+mc alias set local $MINIO_SERVER_HOSTNAME $MINIO_ADMIN_LOGIN $MINIO_ADMIN_PASSWORD
+mc admin user add local $MINIO_LOGIN $MINIO_PASSWORD
+mc mb /mnt/minio-volume/$MINIO_BUCKET_NAME
