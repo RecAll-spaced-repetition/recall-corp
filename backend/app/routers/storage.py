6db7da3698b0876762adeaf7b485ab4b323a7de3
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-11-03 15:47:06 +0700

    Add POST route

--- backend/app/routers/storage.py
+++ backend/app/routers/storage.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile
 from fastapi.responses import FileResponse
+
 import os
 
 from app.storage import storage, bucket_name
+from app.schemas.storage import FileUploadedScheme
 
 router = APIRouter(
     prefix='/storage',
@@ -10,26 +12,33 @@ router = APIRouter(
 )
 
 
-
 @router.get('/{path}', response_class=FileResponse)
 def get_file(path: str):
-    # response = None
-    # try:
-    #   response = m.get_object('recall-bucket', path)
-    #   return response
-    # finally:
-    #   if response != None:
-    #     response.close()
-    #     response.release_conn()
-    metadata = storage.fget_object(bucket_name, path, path) == None
-    if metadata == None:
-        return HTTPException(404, 'File not found')
     try:
+        if storage.fget_object(bucket_name, path, path) == None:
+            raise FileNotFoundError()
         return FileResponse(path)
-    finally:
-        os.remove(path) # Doesn't work at all...
+    except:
+        raise HTTPException(404, 'File not found')
+
+
+@router.post('/{path}', response_model=FileUploadedScheme)
+def add_file(file: UploadFile):
+    try:
+        if storage.stat_object(bucket_name, file.filename) != None:
+            raise HTTPException(409, 'File with this name already exists')
+        result = storage.put_object(bucket_name, file.filename, file.file, file.size)
+        return FileUploadedScheme(
+            url=router.url_path_for('add_file', path=result.object_name)  
+        )
+    except ValueError as e:
+        raise HTTPException(409, 'Failed to upload file: ' + e.message)
 
 
 @router.delete('/{path}')
 def delete_file(path: str):
-    storage.remove_object(bucket_name, path)
+    try:
+        storage.remove_object(bucket_name, path)
+        return {}
+    except ValueError as e:
+        raise HTTPException(404, 'Failed to delete file: ' + e.message)
