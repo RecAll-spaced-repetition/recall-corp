2816d662eade7eb3132d8eb1f8cd2abfda2f7079
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-11-07 02:55:41 +0700

    Import fixes

--- backend/app/crud/storage.py
+++ backend/app/crud/storage.py
@@ -4,12 +4,13 @@ from minio import S3Error
 from minio.datatypes import Object
 from minio.helpers import ObjectWriteResult
 
-from app.storage import storage, bucket_name
+from app.config import minio_settings
+from app.storage import storage
 
 
 def is_file_exists(path_to_object: str) -> bool:
     try:
-        if storage.stat_object(bucket_name, path_to_object) != None:
+        if storage.stat_object(minio_settings.BUCKET_NAME, path_to_object) != None:
             return True
         else:
             return False
@@ -18,7 +19,7 @@ def is_file_exists(path_to_object: str) -> bool:
 
 
 def get_file_stream(full_path: str) -> Generator[bytes, any, None]:
-    file_response = storage.get_object(bucket_name, full_path)
+    file_response = storage.get_object(minio_settings.BUCKET_NAME, full_path)
     async def __file_stream_generator():
         try:
             for chunk in file_response.stream():
@@ -30,12 +31,12 @@ def get_file_stream(full_path: str) -> Generator[bytes, any, None]:
 
 
 def get_files_list(user_id: int) -> Iterator[Object]:
-    return storage.list_objects(bucket_name, f'{user_id}/')
+    return storage.list_objects(minio_settings.BUCKET_NAME, f'{user_id}/')
 
 
 def upload_file(full_path: str, data: BinaryIO, size: int) -> ObjectWriteResult:
-    return storage.put_object(bucket_name, full_path, data, size)
+    return storage.put_object(minio_settings.BUCKET_NAME, full_path, data, size)
 
 
 def delete_file(full_path: str):
-    storage.remove_object(bucket_name, full_path)
+    storage.remove_object(minio_settings.BUCKET_NAME, full_path)
--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -3,12 +3,8 @@ from typing import Annotated
 from fastapi import Depends, Body
 from sqlalchemy.ext.asyncio import AsyncConnection
 
-from minio import Minio
-
 from app.database import get_async_connection
 
 
 DBConnection = Annotated[AsyncConnection, Depends(get_async_connection)]
 IntList = Annotated[list[int], Body]
-
-StorageConnection = Annotated[Minio, Depends(get_storage)]
