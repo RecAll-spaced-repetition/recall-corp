2bd161d770bd2c7daf66e8edd0d709dbc34fbb52
Anton Tsoy <tcoy210@gmail.com>
2024-11-07 08:09:16 +0700

    Wrote basement for custom auth implementation

---
+++ backend/app/auth/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AuthSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="./config/backend.env")
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    @property
+    def get_auth_data(self) -> dict[str, str]:
+        return {"secret_key": self.SECRET_KEY, "algorithm": self.ALGORITHM}
+
+
+authSettings: AuthSettings = AuthSettings()
---
+++ backend/app/auth/utils.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt
+from passlib.context import CryptContext
+
+from app.auth.config import authSettings
+
+
+__pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return __pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return __pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode: dict = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=30)
+    to_encode.update({"exp": expire})
+    auth_data: dict = authSettings.get_auth_data
+    encode_jwt = jwt.encode(to_encode, auth_data['secret_key'], algorithm=auth_data['algorithm'])
+    return encode_jwt
