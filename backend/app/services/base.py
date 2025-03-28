a7430ecd0ba444425a15e9d719553dd8280de5b1
Anton Tsoy <tcoy210@gmail.com>
2025-03-29 00:47:01 +0700

    Wrote decorator for clearing boilerplate context manager

--- backend/app/services/base.py
+++ backend/app/services/base.py
@@ -3,7 +3,14 @@ from abc import ABC
 from app.db import UnitOfWorkDep
 
 
-__all__ = ["BaseService"]
+__all__ = ["BaseService", "with_unit_of_work"]
+
+
+def with_unit_of_work(method):
+    async def wrapper(self, *args, **kwargs):
+        async with self.uow.begin():
+            return await method(self, *args, **kwargs)
+    return wrapper
 
 
 class BaseService(ABC):
--- backend/app/services/card.py
+++ backend/app/services/card.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 from app.repositories import CardCollectionRepository, UserRepository, CardRepository
 from app.schemas import Card, CardCreate, CollectionShort
 
-from .base import BaseService
+from .base import BaseService, with_unit_of_work
 
 
 __all__ = ["CardService"]
@@ -37,46 +37,46 @@ class CardService(BaseService):
         if new_collections := list(request_collections.difference(old_collections)):
             await card_collection_repo.set_card_collection_connections(card_id, new_collections)
 
+    @with_unit_of_work
     async def add_card(self, user_id: int, collections: list[int], card: CardCreate) -> Card:
-        async with self.uow.begin():
-            if not await self.uow.get_repository(UserRepository).exists_user_with_id(user_id):
-                raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
-            card_data = card.model_dump()
-            card_data["owner_id"] = user_id
-            new_card = await self.uow.get_repository(CardRepository).create_one(card_data, Card)
-            await self.__create_connections(
-                self.uow.get_repository(CardCollectionRepository), user_id, new_card.id, collections)
-            return new_card
+        if not await self.uow.get_repository(UserRepository).exists_user_with_id(user_id):
+            raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
+        card_data = card.model_dump()
+        card_data["owner_id"] = user_id
+        new_card = await self.uow.get_repository(CardRepository).create_one(card_data, Card)
+        await self.__create_connections(
+            self.uow.get_repository(CardCollectionRepository), user_id, new_card.id, collections)
+        return new_card
 
+    @with_unit_of_work
     async def get_card(self, card_id: int) -> Card:
-        async with self.uow.begin():
-            card = await self.uow.get_repository(CardRepository).get_card_by_id(card_id, Card)
-            if card is None:
-                raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
-            return card
+        card = await self.uow.get_repository(CardRepository).get_card_by_id(card_id, Card)
+        if card is None:
+            raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
+        return card
 
+    @with_unit_of_work
     async def get_card_collections(self, user_id: int, card_id: int) -> list[CollectionShort]:
-        async with self.uow.begin():
-            card_repo = self.uow.get_repository(CardRepository)
-            if not await card_repo.exists_card_with_owner(user_id, card_id):
-                raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
-            card_collection_repo = self.uow.get_repository(CardCollectionRepository)
-            return await card_collection_repo.get_card_collections(card_id, CollectionShort)
+        card_repo = self.uow.get_repository(CardRepository)
+        if not await card_repo.exists_card_with_owner(user_id, card_id):
+            raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
+        card_collection_repo = self.uow.get_repository(CardCollectionRepository)
+        return await card_collection_repo.get_card_collections(card_id, CollectionShort)
 
+    @with_unit_of_work
     async def update_user_card(
             self, user_id: int, card_id: int, new_card: CardCreate, collections: list[int]
     ) -> Card:
-        async with self.uow.begin():
-            card_repo = self.uow.get_repository(CardRepository)
-            if not await card_repo.exists_card_with_owner(user_id, card_id):
-                raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
-            await self.__update_connections(
-                self.uow.get_repository(CardCollectionRepository), user_id, card_id, collections)
-            return await card_repo.update_card_by_id(card_id, new_card.model_dump(), Card)
+        card_repo = self.uow.get_repository(CardRepository)
+        if not await card_repo.exists_card_with_owner(user_id, card_id):
+            raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
+        await self.__update_connections(
+            self.uow.get_repository(CardCollectionRepository), user_id, card_id, collections)
+        return await card_repo.update_card_by_id(card_id, new_card.model_dump(), Card)
 
+    @with_unit_of_work
     async def delete_card(self, user_id: int, card_id: int) -> None:
-        async with self.uow.begin():
-            card_repo = self.uow.get_repository(CardRepository)
-            if not await card_repo.exists_card_with_owner(user_id, card_id):
-                raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
-            await card_repo.delete_card(card_id)
+        card_repo = self.uow.get_repository(CardRepository)
+        if not await card_repo.exists_card_with_owner(user_id, card_id):
+            raise HTTPException(status_code=400)  ## ТУТ ДОЛЖНО БЫТЬ КАСТОМНОЕ ИСКЛЮЧЕНИЕ!
+        await card_repo.delete_card(card_id)
