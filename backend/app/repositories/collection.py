15a6372127009a464680f40936c987f013d9c0fd
Anton Tsoy <tcoy210@gmail.com>
2025-05-12 13:48:29 +0700

    Okay Kirill was right with his sql expressions style

--- backend/app/repositories/collection.py
+++ backend/app/repositories/collection.py
@@ -31,7 +31,8 @@ class CollectionRepository(BaseSQLAlchemyRepository):
             self, user_id: int | None, output_schema: Type[SchemaType], limit: int, offset: int
     ) -> list[SchemaType]:
         result = await self.connection.execute(
-            select(self.table.c[*output_schema.fields()]).where(or_(
+            select(self.table.c[*output_schema.fields()])
+            .where(or_(
                 self.table.c.is_public.is_(True),
                 self.table.c.owner_id == user_id
             )).limit(limit).offset(offset)
