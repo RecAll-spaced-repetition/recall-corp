2e2824e299d4789a4427799bcbf1b45679e3b2b3
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2026-02-23 15:27:47 +0700

    Add google fonts autodownload conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), tailwindcss()],
+  plugins: [viteReact(), tailwindcss(), webfontDownload()],
   server: {
     strictPort: true,
   },
